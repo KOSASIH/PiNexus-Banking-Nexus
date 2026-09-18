6999efa75c57e1da6ef2a3212c9a34049fad6a84
KOSASIH <kosasihg12@gmail.com>
2026-09-18 07:32:00 +0700

    feat(v1.2.0): omni-dex.ts

--- src/blockchain/dex/omni-dex.ts
+++ src/blockchain/dex/omni-dex.ts
@@ -343,10 +343,10 @@ export class OmniDEX {
 
   private _initializeCorePairs(): void {
     const corePairs = [
-      { base: 'PNX', quote: 'USDT', chainId: 5772156 },
+      { base: 'PNX', quote: 'USDT', chainId: 1202056 },
       { base: 'ETH', quote: 'USDT', chainId: 1 },
-      { base: 'BTC', quote: 'USDT', chainId: 5772156 },
-      { base: 'PNX', quote: 'ETH', chainId: 5772156 },
+      { base: 'BTC', quote: 'USDT', chainId: 1202056 },
+      { base: 'PNX', quote: 'ETH', chainId: 1202056 },
       { base: 'SOL', quote: 'USDT', chainId: 'solana' },
       { base: 'DOT', quote: 'USDT', chainId: 'polkadot' },
     ];
